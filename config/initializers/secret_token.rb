52bf989f11466fa952182df47d8520a8b1da8c35
ScottBigbee <scottbigbee@hotmail.com>
2014-03-19 19:25:20 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PaperclipTest::Application.config.secret_key_base = 'd9b45e528a48223acdd614ba08c1aad3d7521fae67b21a0e2e3af7f59c9b7609781c7e8551940b89ad1a390744fdab256911f2900648a756900d0aa348919324'
